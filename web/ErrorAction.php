58c94d3129a157d05aaea5a826318f851583cac7
Will Wharton <willmwharton@gmail.com>
2015-08-17 12:04:41 -0700

    custom error action that will log excpetions

---
+++ web/ErrorAction.php
@@ -0,0 +1,43 @@
+<?php
+namespace wartron\yii2eventlog\web;
+
+use Yii;
+use yii\base\Exception;
+use yii\base\UserException;
+use yii\web\ErrorAction as BaseErrorAction;
+use wartron\yii2eventlog\models\EventlogItem;
+
+class ErrorAction extends BaseErrorAction
+{
+    public function run()
+    {
+        $exception = Yii::$app->getErrorHandler()->exception;
+        if($exception)
+        {
+
+            $data = [];
+
+            if ($exception instanceof HttpException) {
+                $data['http']   =   true;
+                $data['code']   =   $exception->statusCode;
+            } else {
+                $data['http']   =   false;
+                $data['code']   =   $exception->getCode();
+            }
+
+            $data['name']   =   $exception->getName();
+
+            $data['message'] = $exception->getMessage();
+
+            // new user Event
+            $eli = new EventlogItem();
+            $eli->name = "System Exception";
+            $eli->type = EventlogItem::SYSTEM_EXCEPTION;
+            $eli->data = json_encode($data);
+            $eli->saveOrThrow();
+        }
+        return parent::run();
+
+    }
+
+}
